8b9c8d89b73bd39245f67f3aff719f279065257d
Fatimah Alghamdi <fatimah-ma@hotmail.com>
2022-07-17 13:05:24 +0300

    first java application

---
+++ FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Fatimah Mohammed");
+        System.out.println("I'm 100 years old");
+        System.out.println("My hometown is Dammam, KSA");
+
+    }
+    
+}
